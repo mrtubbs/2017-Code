bf7fc2d8795bb38291e468244b565a91593ecf07
McTschecker <10790084+McTschecker@users.noreply.github.com>
2017-10-10 16:45:26 -0400

    comments on init

--- src/org/usfirst/frc/team1414/robot/Robot.java
+++ src/org/usfirst/frc/team1414/robot/Robot.java
@@ -27,12 +27,12 @@ public class Robot extends IterativeRobot {
 	String autoSelected;
 	SendableChooser<String> chooser = new SendableChooser<>(); //-∞/10
 
-	@Override
-	public void robotInit() {
-		chooser.addDefault("Default Auto", defaultAuto);
-		chooser.addObject("My Auto", customAuto);
-		SmartDashboard.putData("Auto choices", chooser);
-		System.out.println("01001101 01100001 01100001 01101110 01101001 01110100 00100000 01001101 01100001 01100100 01100001 01101110");
+	@Override //overide
+	public void robotInit() { //init void
+		chooser.addDefault("Default Auto", defaultAuto); //part of the init
+		chooser.addObject("My Auto", customAuto); //add object
+		SmartDashboard.putData("Auto choices", chooser); //put data
+		System.out.println("01001101 01100001 01100001 01101110 01101001 01110100 00100000 01001101 01100001 01100100 01100001 01101110"); //printing crazzzzzzyyyyyyy
 	}
 
 	@Override
