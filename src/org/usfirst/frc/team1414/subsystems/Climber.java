b400e7f42a4a36d21ee1a5bb1e53a1ec8e9332cb
McTschecker <10790084+McTschecker@users.noreply.github.com>
2017-10-10 17:02:53 -0400

    kommants

--- src/org/usfirst/frc/team1414/subsystems/Climber.java
+++ src/org/usfirst/frc/team1414/subsystems/Climber.java
@@ -1,45 +1,45 @@
 package org.usfirst.frc.team1414.subsystems;
 
-import com.ctre.CANTalon;
-import org.usfirst.frc.team1414.subsystems.Intake;
+import com.ctre.CANTalon; //importing stuff
+import org.usfirst.frc.team1414.subsystems.Intake; //and more stuff
 
 public class Climber extends Subsystem {
 
-	public CANTalon climberMotorMaster, climberMotorSlave;
+	public CANTalon climberMotorMaster, climberMotorSlave; //public thing
 	
 
 	
-	public Climber() {
-		climberMotorMaster = new CANTalon(3);
-		climberMotorSlave = new CANTalon(9);
+	public Climber() { //function to climb
+		climberMotorMaster = new CANTalon(3); //start motor instance
+		climberMotorSlave = new CANTalon(9); //start motor slave instance
 		
 	}
 	
 	public void startClimb() {
-		climberMotorMaster.set(-1.0);
-		climberMotorSlave.set(1.0);
+		climberMotorMaster.set(-1.0); //start motor
+		climberMotorSlave.set(1.0); //start motor
 
 	}
 	
 	public void stopClimb() {
-		climberMotorMaster.set(0.0);
-		climberMotorSlave.set(0.0);
+		climberMotorMaster.set(0.0); //stop motor
+		climberMotorSlave.set(0.0);//stop motor
 	}
 	
 	@Override
-	public void outputToSmartDashboard() {
+	public void outputToSmartDashboard() { //output with nothing to do
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void stop() {
+	public void stop() { //no stop
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void zeroSensors() {
+	public void zeroSensors() { //nothing
 		// TODO Auto-generated method stub
 		
 	}
